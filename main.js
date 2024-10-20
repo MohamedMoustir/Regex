5f7b70116bc02a93ee90a7d5300812d442686ca3
MohamedMoustir <itsmoustir@gmail.com>
2024-10-20 01:37:16 +0100

    done project

--- main.js
+++ main.js
@@ -4,28 +4,33 @@ function poupup() {
     let input = document.querySelector("#email").value;
 let popup =document.querySelector(".popup");
 let h3 =document.querySelector("h3");
+let password =document.querySelector("#password").value;
+
     const valid =/\w+@\w+\.\w+/g;
- if(input[0]=== ""){
-    popup.innerHTML="input is not valide";
+
+    
+ if(input[0]=== ""||password==""){
+    popup.innerHTML="input is empty";
     popup.style.background='red';
     popup.style.top="0";
-    window.reload()
-
- }else if(valid.test(input)) {
-    popup.innerHTML="input is valid";
+    
+ }else if(valid.test(input)||password=="") {
+    popup.innerHTML="email is valid";
     popup.style.background='green';
     popup.style.top="0";
     } else{
-    popup.innerHTML="input is not valide";
+    popup.innerHTML="email is not valide";
     popup.style.background='red';
     popup.style.top="0";
 
     }
-    
-setTimeout(() => {
-    popup.style.top = "-100px"; // إخفاء الـ popup بعد 3 ثوانٍ
-}, 3000);
 
+   
+    setTimeout(() => {
+        popup.style.top = "-100px"; 
+    }, 3000);
 }
 
- 
+  window.onclick=function(){
+    popup.style.top = "-100px"; 
+  }
